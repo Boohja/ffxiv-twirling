1851ee75b4c139d6892ae122bf5964a636d1eaaf
Boohja <matthias@comasu.de>
2023-09-03 17:26:39 +0200

    adjustments for vercel

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,8 +2,6 @@ import { invalidate } from '$app/navigation'
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
 
-export const prerender = true;
-
 export const load = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
 
--- svelte.config.js
+++ svelte.config.js
@@ -6,17 +6,8 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: false
-		})
+		adapter: adapter({})
 	}
 };
 
