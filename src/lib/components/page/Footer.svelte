3d2f46c99f69db02db6ad6a2b00685c67f981e23
Boohja <matthias@comasu.de>
2026-01-26 23:26:36 +0100

    footer added

--- src/lib/components/page/Footer.svelte
+++ src/lib/components/page/Footer.svelte
@@ -1,5 +1,18 @@
 <div class="bg-gray-900 text-slate-600 py-4">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
-    FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd.
+    <div>
+      © SQUARE ENIX CO., LTD. All Rights Reserved.<br>
+      FINAL FANTASY® and FINAL FANTASY XIV® are registered trademarks of Square Enix Holdings Co., Ltd.<br>
+      This site is a fan project and is not affiliated with or endorsed by Square Enix.
+    </div>
+    <a class="flex items-center gap-4 px-3 py-2 grayscale rounded-lg transition-all duration-300 hover:scale-110 hover:bg-slate-800/30 hover:shadow-lg hover:grayscale-0 hover:shadow-blue-500/20 hover:text-slate-400" href="https://discord.com/users/850980511606505494" target="_blank" rel="noopener noreferrer" aria-label="Contact the developer on Discord">
+      <div class="transition-colors duration-300">
+        Made by Aeron<br>
+        from Phoenix
+      </div>
+      <div>
+        <img src="/images/aeron.webp" alt="Aeron's Logo" class="h-12 w-12 rounded-full inline-block transition-transform opacity-80 duration-500 hover:opacity-100 hover:rotate-12 hover:scale-150" />
+      </div>
+    </a>
   </div>
 </div>
